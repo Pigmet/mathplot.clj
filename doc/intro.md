9317bf033b9a8d2fa5601cfe5934c3c9db23d824
Gou <nakamura462645@gmail.com>
2020-05-17 18:30:40 +0900

    updated project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mathplot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/mathplot/core.clj
@@ -0,0 +1,7 @@
+(ns mathplot.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
