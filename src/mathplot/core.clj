67156e0fe2e24d45d126030a6c55dc7525f594e6
Gou <nakamura462645@gmail.com>
2020-05-18 10:49:38 +0900

    erased comment

--- src/mathplot/core.clj
+++ src/mathplot/core.clj
@@ -214,16 +214,3 @@
   [& args]
   (run))
 
-
-(comment
-
-  (do
-    (swap! state assoc :mode :parameter)
-    (-> (build)
-        add-translate-behavior
-        add-button-behavior      
-        show!))
-
-  )
-
-;; (run)
